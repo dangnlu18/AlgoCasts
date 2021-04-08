4e663e8f3f95128119640118a38fc28927d157c5
dang lu <dangnlu18@gmail.com>
2021-04-07 20:35:02 -0700

    anagrams done

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,49 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+
+    const mapA = makeDict(stringA)
+    const mapB = makeDict(stringB)
+
+    if (Object.keys(mapA).length !== Object.keys(mapB).length){
+        return false
+    }
+    else{
+        for (let char in mapA){
+            if(mapA[char] !== mapB[char]){
+                return false
+            }else{
+                return true
+            }
+        }
+
+    }
+
+    // if (stringA.length !== stringB.length){
+    //     return false
+    // }
+    // else{
+    //     const mapA = makeDict(stringA)
+    //     const mapB = makeDict(stringB)
+        
+    
+       
+    // }
+}
+
+
+
+function makeDict(str){
+    let map = {}
+    for (let element of str.replace(/[^\w]/g, '').toLowerCase()){
+        if(map[element]){
+            map[element] ++
+        }else{
+            map[element] = 1
+        }
+    }
+    return map
+}
 
 module.exports = anagrams;
