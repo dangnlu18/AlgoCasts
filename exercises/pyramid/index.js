b14bf1031871f5baa01032b537076819ac41cd73
dang lu <dangnlu18@gmail.com>
2021-04-15 12:57:16 -0700

    pyramid done

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,48 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// length of stair is 2n - 1
+
+
+// n= 3
+
+
+
+function pyramid(n, row=0, stair = '') {
+
+
+    if (row === n){
+        return
+    }
+
+
+    if (stair.length === 2*n - 1){
+        console.log(stair)
+        return pyramid(n, row+1)
+    }
+
+    const mid = Math.floor((2*n -1)/2)
+
+    let add;
+
+    if (mid - row <= stair.length &&  mid + row >= stair.length){
+        add = '#'
+    }
+    else{
+        add = ' '
+    }
+    
+    pyramid(n, row, stair+ add)
+
+}
+
+
+// function createMatrix(n){
+//     let result = []
+//     for (let i=0; i<n; i++){  
+//         result.push(new Array(n).fill(' '))
+//     }
+//     return result
+// }
 
 module.exports = pyramid;
