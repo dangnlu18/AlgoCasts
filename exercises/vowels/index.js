58747500902bc1cb55fa6bb6bb45e9ecb8f22a0c
dang lu <dangnlu18@gmail.com>
2021-04-15 16:22:48 -0700

    find vowels exercise

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,28 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    const map = makeCharMap(str)
+    let count = 0
+    
+    for (let key of str){
+        if (key ==='a' || key === 'e' || key==='i' || key ==='o' || key ==='u'){
+            count += map[key]
+        }
+    }
+    return count
+
+}
+
+
+
+function makeCharMap(str){
+    let map = {}
+    str= str.toLowerCase()
+    for (let char of str){
+        map[char] = map[char] +1 || 1 
+    }
+    return map
+}
 
 module.exports = vowels;
