5015f41ac3cac211937df2fcc526cfe2db906de5
dang lu <dangnlu18@gmail.com>
2021-04-15 21:49:15 -0700

    spiral matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,74 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+
+
+// row 0 col 0, row 0 col1 , row 0 col2 
+
+// row 1 col 2, row 2 col 2, row 2 col 1
+
+// row 2 col 0, row 1 col 0, row 1 col 1
+
+function matrix(n) {
+    let matrix = makeEmptyMatrix(n)
+    let counter = 1
+    let start_row = 0
+    let end_row = n-1
+    let start_col = 0
+    let end_col = n-1
+
+
+    while (start_col <= end_col && start_row <= end_row){
+        // top row
+        for ( let i= start_col; i<= end_col; i++){
+            matrix[start_row][i] = counter
+            counter +=1
+        }
+        start_row ++
+        
+        //last column
+
+        for (let i = start_row ; i <= end_row ; i++){
+            matrix[i][end_col] = counter;
+            counter ++
+        }
+
+        //bottom side
+        end_col --
+
+        for (let i = end_col; i >= start_col; i--){
+            matrix[end_row][i] = counter
+            counter ++
+        }
+
+        // left hand side
+        end_row --
+
+        for (let i = end_row; i >= start_row;  i--){
+            matrix[i][start_col] = counter;
+            counter ++
+        }
+
+        start_col ++
+
+    }
+
+    return matrix
+}
+
+
+
+
+function makeEmptyMatrix(n){
+    let matrix = []
+
+    for (let i = 0; i<n; i++){
+        matrix.push(new Array(n).fill(0))
+    }
+    return matrix
+
+}
 
 module.exports = matrix;
+
+
